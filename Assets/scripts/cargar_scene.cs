de9e7f50436b0a9005c348aaa9a17778a4522287
Code0End <lbenitesable@gmail.com>
2022-03-22 13:46:11 -0600

    Práctica 4

---
+++ Assets/scripts/cargar_scene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class cargar_scene : MonoBehaviour
+{
+   public void CargarEscena(string nombreEscena)
+    {
+        SceneManager.LoadScene(nombreEscena);
+    }
+}
