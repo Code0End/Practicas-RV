35d9462e698036c18cacd678b7e74762aa87e1e6
Code0End <lbenitesable@gmail.com>
2022-03-29 09:45:20 -0600

    practica 5

---
+++ Assets/scripts/fps.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fps : MonoBehaviour
+{
+    public float sensX;
+    public float sensY;
+    public float speed = 0.0f;
+
+    float xRotation;
+    float yRotation;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        rb = gameObject.GetComponent<Rigidbody>();
+     
+    }
+
+  
+    // Update is called once per frame
+    void Update()
+    {
+        mirar();
+        moverp();
+        
+
+    }
+
+    void mirar()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        Camera.main.transform.localRotation = Quaternion.Euler(xRotation, yRotation+180, 0);
+
+    }
+
+    void moverp()
+    {
+        Vector2 axis = new Vector2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal")) * speed;
+        Vector3 forward = new Vector3(-Camera.main.transform.right.z, 0.0f, Camera.main.transform.right.x);
+        Vector3 wishDirection = (forward * axis.x + Camera.main.transform.right * axis.y + Vector3.up * rb.velocity.y);
+        rb.velocity = wishDirection;
+    }
+}
---
+++ Assets/scripts/reloj.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class reloj : MonoBehaviour
+{
+    public GameObject texto;
+    public int segundos = 300;
+    public bool disminuyendo = true;
+    bool on = true;
+
+    void Start()
+    {
+        texto.SetActive(false);
+    }
+
+    void Update()
+    {
+         if(disminuyendo==false && segundos > 0 && on == true)
+        {
+            StartCoroutine(crono());
+        } 
+        if (segundos == 0)
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.CompareTag("c_e"))
+        {
+            
+            disminuyendo = false;
+            texto.SetActive(true);
+        }
+        if (c.gameObject.CompareTag("c_s"))
+        {
+            disminuyendo = true;
+            on = false;
+        }
+        if (c.gameObject.CompareTag("c_f"))
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
+    }
+
+
+        IEnumerator crono()
+    {
+        disminuyendo = true;
+        yield return new WaitForSeconds(1);
+        segundos -= 1;
+        float minutos = Mathf.FloorToInt(segundos / 60);
+        float displaysegundos = Mathf.FloorToInt(segundos % 60);
+        texto.GetComponent<Text>().text = minutos+":"+displaysegundos;
+        disminuyendo = false;
+    }
+
+    
+}
